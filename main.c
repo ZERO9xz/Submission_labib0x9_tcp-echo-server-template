6b578e431bed37703f3fed20420b625facd2e85c
labibalfaisal9834-ux <labibalfaisal9834@gmail.com>
2026-08-17 12:43:13 +0600

    Refactor main.c for TCP echo server implementation

--- main.c
+++ main.c
@@ -1,34 +1,72 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<string.h>
-#include<stdbool.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
-#include<perror.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
-void die(int err, char* msg) {
-    if (err == -1) {
-        perror(msg);
-    }
-}
+#define PORT 8080
+#define BUFFER_SIZE 1024
 
 int main() {
+    int server_fd, client_fd;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t addr_len = sizeof(client_addr);
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_received;
 
-    // To-Do: TCP socket create
+    // 1. Create socket (IPv4, TCP)
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
 
-    // To-Do: Socket Binding
+    // Allow socket address reuse to prevent "Address already in use" errors on restart
+    int opt = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-    // To-Do: Listening
+    // 2. Bind socket to port and IP
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY; // Listen on all network interfaces
+    server_addr.sin_port = htons(PORT);
 
-    // To-Do: Receiving Loop
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Bind failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
 
-    // To-Do: Accept Client Connection
+    // 3. Listen for incoming connections
+    if (listen(server_fd, 5) < 0) {
+        perror("Listen failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
 
-    // To-Do: Read from client
+    printf("Echo server running on port %d...\n", PORT);
 
-    // To-Do: Send to client
+    // 4. Accept connection from client
+    if ((client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &addr_len)) < 0) {
+        perror("Accept failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Client connected: %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+    // 5. Echo loop: read data and send it straight back
+    while ((bytes_received = recv(client_fd, buffer, sizeof(buffer), 0)) > 0) {
+        send(client_fd, buffer, bytes_received, 0);
+    }
+
+    if (bytes_received == 0) {
+        printf("Client disconnected.\n");
+    } else if (bytes_received < 0) {
+        perror("Read error");
+    }
+
+    // 6. Clean up
+    close(client_fd);
+    close(server_fd);
 
     return 0;
 }
